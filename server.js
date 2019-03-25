b13ead10905bc83bc6cc549b8ecf12477e087728
Hossein Alali <hossein.alali@chasacademy.se>
2019-03-25 15:43:28 +0100

    add routes for application

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const admin = require('./routes/api/admin');
+const users = require('./routes/api/users');
+const products = require('./routes/api/products');
+const orders = require('./routes/api/orders');
+const profile = require('./routes/api/profile');
 
 const app = express();
 
 // Database config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello!"));
+app.get('/', (req, res) => res.send('Hello!'));
+
+// Use Routes
+app.use('/api/admin', admin);
+app.use('/api/users', users);
+app.use('/api/products', products);
+app.use('/api/orders', orders);
+app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000;
 
