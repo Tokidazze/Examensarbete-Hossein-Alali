6b320d250d7b2e908577b2c2fb21d14f0fbe7ab6
Tokidaze <hosseinalali@chasacademy.se>
2019-03-26 15:49:28 +0100

    add orderProducts and product model

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  avalaible: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  stock: {
+    type: Number
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Products = mongoose.model('products', ProductSchema);
