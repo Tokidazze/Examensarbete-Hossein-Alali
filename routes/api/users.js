0d5f2cbde3474f855d5798537b0ddc2c86546ba0
Tokidaze <hosseinalali@chasacademy.se>
2019-03-26 14:23:59 +0100

    add validation for login and register routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,19 +5,32 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 // Models
 const User = require('../../models/User');
 
 // POST | Register a new user
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: req.body.password,
+        phoneNumber: req.body.phoneNumber
       });
 
       bcrypt.genSalt(10, (err, salt) => {
@@ -36,12 +49,19 @@ router.post('/register', (req, res) => {
 
 // POST | Sign in user
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find user by email
   User.findOne({ email }).then(user => {
-    // Check for user
+    //Check for user
     if (!user) {
       errors.email = 'User not found';
       return res.status(404).json(errors);
@@ -51,7 +71,7 @@ router.post('/login', (req, res) => {
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
         // User Match
-        const payload = { id: user.id, firstName: user.firstName, lastName: user.lastName };
+        const payload = { id: user.id, firstName: user.firstName, lastName: user.lastName }; // Create JWT payload
 
         // Sign Token
         jwt.sign(payload, keys.secretOrKey, { expiresIn: 86400 }, (err, token) => {
