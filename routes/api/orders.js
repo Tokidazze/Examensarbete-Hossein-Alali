aaff0d01b567faf7dfd522d6ae4d0b124cff47a6
Hossein Alali <hossein.alali@chasacademy.se>
2019-03-29 15:29:29 +0100

    remove useless files

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 
 // Load models
-const User = require('../../models/User');
 const Order = require('../../models/Order');
-const Product = require('../../models/Product');
 
 // Validation
 const validateOrderInput = require('../../validation/order');
