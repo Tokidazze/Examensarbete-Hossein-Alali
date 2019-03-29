c117190c0c0a30901f1ee3f512bb1f9779a73714
Hossein Alali <hossein.alali@chasacademy.se>
2019-03-29 17:14:30 +0100

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>My WebShop app</h1>
+        <button className='ui primary button'>Save</button>
+      </div>
+    );
+  }
+}
+
+export default App;
