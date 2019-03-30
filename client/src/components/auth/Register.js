80b5d3e4d64ab8cdeb05475d5a4de79bdd90d972
Hossein Alali <hossein.alali@chasacademy.se>
2019-03-30 15:44:24 +0100

    add validation for register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 import './Auth.css';
 
@@ -31,56 +33,83 @@ class Login extends Component {
       password2: this.state.password2
     };
 
-    console.log(newUser);
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div className='register'>
         <div className='container'>
           <div className='row'>
             <div className='col-md-8 m-auto'>
               <h1 className='display-4 text-center register-header'>Sign Up</h1>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className='form-group'>
                   <input
                     type='text'
-                    className='form-control form-control-lg'
+                    // classnames will always show, is-invalid class will only show if errors.name is triggered
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     placeholder='Name'
                     name='name'
                     value={this.state.name}
                     onChange={this.onChange}
                   />
+                  {/* if errors is triggered show this invalid feedback message through div */}
+                  {errors.name && (
+                    <div className='invalid-feedback'>{errors.name}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='email'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     placeholder='Email'
                     name='email'
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && (
+                    <div className='invalid-feedback'>{errors.email}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='password'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder='Password'
                     name='password'
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className='invalid-feedback'>{errors.password}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='password'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
                     placeholder='Confirm Password'
                     name='password2'
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
+                  {errors.password2 && (
+                    <div className='invalid-feedback'>{errors.password2}</div>
+                  )}
                 </div>
                 <input type='submit' className='btn btn-info btn-block mt-4' />
               </form>
