1da06bf3f98932c7aa304c50b7c9852ea95e08f2
Hossein Alali <hossein.alali@chasacademy.se>
2019-05-13 15:31:44 +0200

    fix footer

--- client/src/App.js
+++ client/src/App.js
@@ -65,59 +65,65 @@ class App extends Component {
         <Router>
           <div className='App'>
             <Navbar />
-            <Route exact path='/' component={Landing} />
-            <Switch>
-              {/* Admin Routes */}
-              <PrivateRoute
-                exact
-                path='/admin/dashboard'
-                component={AdminDashboard}
-              />
-              <PrivateRoute exact path='/admin/orders' component={AdminOrder} />
-              <PrivateRoute
-                exact
-                path='/admin/products'
-                component={AdminProduct}
-              />
-              <PrivateRoute
-                exact
-                path='/admin/products/create'
-                component={AdminCreateProduct}
-              />
-              <PrivateRoute
-                exact
-                path='/admin/products/:id'
-                component={AdminEditProduct}
-              />
-              <PrivateRoute exact path='/admin/users' component={AdminUser} />
-              <PrivateRoute
-                exact
-                path='/admin/users/:id'
-                component={AdminEditUser}
-              />
-              {/* Order Routes */}
-              <PrivateRoute
-                exact
-                path='/checkout/order'
-                component={OrderPage}
-              />
-              {/* Payment Routes*/}
-              <PrivateRoute
-                exact
-                path='/checkout/order/payment'
-                component={Payment}
-              />
-              {/* User Routes */}
-              <PrivateRoute exact path='/user/profile' component={UserPage} />
-            </Switch>
-            {/* Product Routes */}
-            <Route exact path='/products' component={Products} />
-            <Route exact path='/products/:id' component={ProductPage} />
-            {/* Auth Routes */}
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
+            <div className='content'>
+              <Route exact path='/' component={Landing} />
+              <Switch>
+                {/* Admin Routes */}
+                <PrivateRoute
+                  exact
+                  path='/admin/dashboard'
+                  component={AdminDashboard}
+                />
+                <PrivateRoute
+                  exact
+                  path='/admin/orders'
+                  component={AdminOrder}
+                />
+                <PrivateRoute
+                  exact
+                  path='/admin/products'
+                  component={AdminProduct}
+                />
+                <PrivateRoute
+                  exact
+                  path='/admin/products/create'
+                  component={AdminCreateProduct}
+                />
+                <PrivateRoute
+                  exact
+                  path='/admin/products/:id'
+                  component={AdminEditProduct}
+                />
+                <PrivateRoute exact path='/admin/users' component={AdminUser} />
+                <PrivateRoute
+                  exact
+                  path='/admin/users/:id'
+                  component={AdminEditUser}
+                />
+                {/* Order Routes */}
+                <PrivateRoute
+                  exact
+                  path='/checkout/order'
+                  component={OrderPage}
+                />
+                {/* Payment Routes*/}
+                <PrivateRoute
+                  exact
+                  path='/checkout/order/payment'
+                  component={Payment}
+                />
+                {/* User Routes */}
+                <PrivateRoute exact path='/user/profile' component={UserPage} />
+              </Switch>
+              {/* Product Routes */}
+              <Route exact path='/products' component={Products} />
+              <Route exact path='/products/:id' component={ProductPage} />
+              {/* Auth Routes */}
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </div>
             <ChatContainer />
-            {/* <Footer /> */}
+            <Footer />
           </div>
         </Router>
       </Provider>
