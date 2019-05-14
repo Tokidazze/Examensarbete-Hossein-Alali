f60527e5e812e43f63f671899fac9e5f9e33eb8b
Hossein Alali <hossein.alali@chasacademy.se>
2019-05-14 15:39:18 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const admin = require('./routes/api/admin');
 const users = require('./routes/api/users');
@@ -34,6 +35,16 @@ app.use('/api/admin', admin);
 app.use('/api/users', users);
 app.use('/api/products', products);
 app.use('/api/orders', orders);
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
