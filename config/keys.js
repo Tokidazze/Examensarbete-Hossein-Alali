368938f176ed63e4de371810cd29670cb448bf03
Hossein Alali <hossein.alali@chasacademy.se>
2019-05-14 15:47:57 +0200

    prod keys

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:hejhej123@ds121996.mlab.com:21996/chatshop',
+  secretOrKey: 'secret'
+};
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stripePublishableKey: 'pk_test_d9J5PqHSVu8alhM0ujRSP65R00OOoVW2cr',
+  stripeSecretKey: 'sk_test_ACC53YkxOKvJhReQKoVio1JP00YawGn872'
+};
